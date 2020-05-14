622d4575f3baa7c9b50f1fe6c1363257965b0ce1
Tom Tarpey <tomtarpeydev@gmail.com>
2020-05-14 17:15:24 +0100

    enabled login temporarily

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from django.conf.urls import url
 
 urlpatterns = [
-    # path('', include('rest_auth.urls')),
+    path('', include('rest_auth.urls')),
     # path('registration/', include('rest_auth.registration.urls')),
 ]
